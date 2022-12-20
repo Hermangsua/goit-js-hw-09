42707a8196d76ce2c48a4e7b0b508acadc3fb728
Hermangsua <113884950+Hermangsua@users.noreply.github.com>
2022-12-20 09:46:14 +0000

    Deploying to gh-pages from @ Hermangsua/goit-js-hw-09@1cd31cc2b6fd0c575ee83b3422f673d29f6400ad 🚀

---
+++ 01-color-switcher.3dfe1135.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(d=>{timerId=null,t.disabled=!0,e.disabled=!1,timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(d=>{t.disabled=!1,e.disabled=!0,clearTimeout(timerId)}));
+//# sourceMappingURL=01-color-switcher.3dfe1135.js.map
